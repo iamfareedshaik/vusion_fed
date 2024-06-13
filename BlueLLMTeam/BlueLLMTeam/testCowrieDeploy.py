773a4543f9c8c34d405c11943750c8c54c7cc1e2
iamfareedshaik <iamfareedshaik@gmail.com>
2024-06-13 04:07:48 -0500

    added

---
+++ BlueLLMTeam/BlueLLMTeam/testCowrieDeploy.py
@@ -0,0 +1,8 @@
+from LLMEndpoint import ChatGPTEndpoint
+from RoleAgent import CowrieDesignerRole
+
+endpoint = ChatGPTEndpoint()
+
+designer = CowrieDesignerRole(endpoint)
+
+designer.deploy_honeypot(designer.create_honeypot("Cowrie Honeypot"))
